1b1cb83d2c9a146221d7659c6528cf0b6fd800b5
rogafe <niamor.rogafe@gmail.com>
2017-01-27 23:20:43 +0000

    Modify

--- auto_install.sh
+++ auto_install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 echo "Auto install of all my need tool for a server"
-
+echo "le script doit etre en root/sudo"
+apt-get update -y
+apt-get upgrade -y
+apt-get install git curl htop 
 
 
 
