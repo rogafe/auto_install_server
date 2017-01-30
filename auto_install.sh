a51176ee3ed7ede6f574241ac6701ea3a9df8d9b
Romain <niamor.rogafe@gmail.com>
2017-01-30 20:50:28 +0100

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 echo "Auto install of all my need tool for a server"
-echo "le script doit etre en root/sudo"
+echo "Le script doit etre en root/sudo"
 apt-get update -y
 apt-get upgrade -y
-apt-get install git curl htop 
-
-
-
-
-
+apt-get install git curl htop -y
+echo"Install antigen .zshrc conf"
+cd /tmp
+git clone https://github.com/rogafe/server_dotefile.git
+mv antigen ~/.antigen
+source ~/.antigen/antigen.zsh
+mv -r zshrc ~/.zshrc
+echo "Install of antigen and .zshrc conf done"
+echo "Docker install"
+curl -sSL https://get.docker.com/ | sh
