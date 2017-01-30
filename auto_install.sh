cea658633953ec062c4deb61435c2edc5ec3fdf3
Romain <niamor.rogafe@gmail.com>
2017-01-30 21:07:11 +0100

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -3,7 +3,7 @@ echo "Auto install of all my need tool for a server"
 echo "Le script doit etre en root/sudo"
 apt-get update -y
 apt-get upgrade -y
-apt-get install git curl htop -y
+apt-get install zsh git curl htop -y
 echo"Install antigen .zshrc conf"
 cd /tmp
 git clone https://github.com/rogafe/server_dotefile.git
