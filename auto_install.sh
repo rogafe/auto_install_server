308a397fef1d988c1c21ecaa34f1c7368067f796
rogafe <niamor.rogafe@gmail.com>
2017-07-20 00:27:00 +0200

    Add byobu to the install program.

--- auto_install.sh
+++ auto_install.sh
@@ -3,12 +3,12 @@ echo "Auto install of all my need tool for a server"
 echo "Le script doit etre en root/sudo"
 apt-get update -y
 apt-get upgrade -y
-apt-get install zsh git curl htop -y
+apt-get install zsh git curl htop byobu -y
 mkdir -p ~/.antigen
 curl -L git.io/antigen > ~/.antigen/antigen.zsh
 chsh -s /bin/zsh
 cd /tmp
 wget https://raw.githubusercontent.com/rogafe/server_dotefile/master/zshrc
 mv zshrc ~/.zshrc
-cd 
+cd
 echo "Install dotfile done, need restart the shell"
