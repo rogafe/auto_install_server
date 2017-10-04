17f13f5242f587db9249db6a0406a88ef6887260
Romain <rogafe@users.noreply.github.com>
2017-10-04 10:44:13 +0200

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -5,9 +5,9 @@ if [[ $EUID -ne 0 ]]; then
   echo "You must be a root user" 2>&1
   exit 1
 else
-echo "updating"; sleep 1
+echo "[updating]"; sleep 1
 apt-get update
-echo "upgrading"; sleep 1
+echo "[upgrading]"; sleep 1
 apt-get upgrade -y
 echo 'Install of needed package "zsh git curl htop byobu vim"';sleep 01
 apt-get install zsh git curl htop byobu vim -y
@@ -20,13 +20,13 @@ wget https://raw.githubusercontent.com/rogafe/server_dotefile/master/zshrc
 mv zshrc ~/.zshrc
 echo "install of antigen done"
 cd /tmp
-echo "installing docker "
+echo "[installing docker]"
 curl -fsSL test.docker.com -o test-docker.sh && sh test-docker.sh
-echo "docker installed "
+echo "[docker installed]"
 sleep 2
-echo "installing spf13-vim"
+echo "[installing spf13-vim]"
 curl http://j.mp/spf13-vim3 -L -o - | sh
-echo "all done closing the ssh shell"
+echo "[all done closing the ssh shell]"
 sleep 2
 exit
 fi
