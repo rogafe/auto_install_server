e2b2e851bb5a8bbea0f9bae8205473b46c4dbf8e
rogafe <rogafe@rogafe.pw>
2018-01-23 16:37:34 +0100

    change in auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -39,14 +39,16 @@ echo "[?] spf13-vim "
 read -p "Install spf13-vim ? (y/n)"
 case ${answer:0:1} in
     y|Y )
-        echo "[installing spf13-vim]"
-        curl http://j.mp/spf13-vim3 -L -o - | sh
-        echo "[all done closing the ssh shell]"
-        echo "Some time shell not work after this so I  close the remote shell"
-        sleep 2
-        exit
-        * )
+          echo "[installing spf13-vim]"
+          curl http://j.mp/spf13-vim3 -L -o - | sh
+          echo "[all done closing the ssh shell]"
+          echo "Some time shell not work after this so I  close the remote shell"
+          sleep 2
+          exit
+    * )
+           echo No
 esac
-exit
+
+           exit
 
 fi
